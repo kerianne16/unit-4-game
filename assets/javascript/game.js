5b059b20f8e10f650ccd28ba0ae0e1bcd1033677
kerianne16 <kerianne116@gmail.com>
2019-04-09 19:03:40 -0400

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,3 +3,21 @@ var win;
 var lost;
 
 
+for (var i = 0; i < 4; i++) {
+
+    var random = Math.floor(Math.random() * 12)
+    console.log(random);
+
+    var crystal = ("<div>");
+
+    crystal.attr({
+        "class": 'crystal',
+        "data-random": random
+    });
+
+    $(".crystal").append(crystal);
+
+
+
+
+}
